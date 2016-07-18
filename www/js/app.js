798a731ff51d915b8528629060c8bbd45e890d4d
nbgdnikola <nikmilicc@gmail.com>
2016-07-18 21:29:15 +0200

    root

--- www/js/app.js
+++ www/js/app.js
@@ -72,6 +72,6 @@ angular.module('starter', ['ionic'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/naslovna');
 
 });
