e3c273839a37991a35c97530d3e842ac20602dce
nbgdnikola <nikmilicc@gmail.com>
2016-07-18 21:10:15 +0200

    sredjen front-end

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -40,41 +40,33 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.naslovna', {
+    url: '/naslovna',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'naslovna': {
+        templateUrl: 'templates/naslovna.html',
+        
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.Onama', {
+      url: '/Onama',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'Onama': {
+          templateUrl: 'templates/Onama.html',
+          
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+   
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.kontakt', {
+    url: '/kontakt',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-kontakt': {
+        templateUrl: 'templates/kontakt.html',
+        
       }
     }
   });
