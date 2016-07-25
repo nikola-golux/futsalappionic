4f6824d4366d2beb1f263ef2ce08770f234a0dfc
nbgdnikola <nikmilicc@gmail.com>
2016-07-25 15:51:13 +0200

    sredjenee rute

--- www/js/auth-interceptor.js
+++
@@ -1,9 +0,0 @@
-angular.module(‘myApp.services’) 
-.factory(‘AuthInterceptor’, function($q) {
-  return {
-    ‘request’: function(config) {
-      config.headers.Authorization = window.localStorage[‘authToken’];
-      return config;
-    }
-  };
-});
