16e77bb9592dc1a94ec842ae730334ffe484fb2a
Petar <petarsijan993@gmail.com>
2016-09-10 14:13:48 +0200

    Sredjene 2 strane za gotov mec

--- www/js/controllers/match_played_controller.js
+++ www/js/controllers/match_played_controller.js
@@ -4,12 +4,22 @@ angular.module('starter.controllers')
 * PLAYER TABS CONTROLLERS
 ****************************************************************************************************/
 // 1) Player_experience controller
-.controller('MatchPlayed_MResulCtrl', function($scope, $http, $state, Matches, HomeTeams, Teams) {
+.controller('MatchPlayed_MResulCtrl', function($scope, $http, $state, Matches, HomeTeams, Teams, MatchPlayers) {
   Matches.query().$promise.then(function(response){
     $scope.id_meca = localStorage.getItem('delegateMatchId');
     $scope.matches = response;
 
     $scope.gotov_mec = getById($scope.matches, $scope.id_meca);
+  });
+    MatchPlayers.query().$promise.then(function(response){
+    $scope.id_meca = localStorage.getItem('delegateMatchId');
+    $scope.match_players = response;
+
+    $scope.home_players_on_match = getAllHomeMatchPlayersByMatchId($scope.match_players, $scope.id_meca);
+  });
+    MatchPlayers.query().$promise.then(function(response){
+    $scope.match_players = response;
+    $scope.away_players_on_match = getAllAwayMatchPlayersByMatchId($scope.match_players, $scope.id_meca);
   });
 })
 // 2) O_nama Controller
