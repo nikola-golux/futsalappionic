5ec8b8512912920cd743610e5b83822b25fd7576
Petar <petarsijan993@gmail.com>
2016-09-14 18:29:01 +0200

    Zavrsen ionic

--- www/js/controllers/league_controller.js
+++ www/js/controllers/league_controller.js
@@ -29,3 +29,26 @@ angular.module('starter.controllers')
 	})
 
 })
+
+.controller('MyLeagueFinishedMatches_Ctrl', function($scope, $state, Leagues, Teams, Matches){
+	Matches.query().$promise.then(function(response){
+		$scope.liga_trenutnog_igraca = localStorage.getItem('playerLeagueId');
+		$scope.matches = response;
+	
+		$scope.finished_matches = getAllFinishedMatchesFromMyLeagueId($scope.matches, $scope.liga_trenutnog_igraca);
+		console.log($scope.finished_matches);
+	})
+
+	$scope.go = function(id){
+      localStorage.setItem('delegateMatchId', id);
+      $state.go('match_played_tab.match_result', {}, {reload: true});
+      /*$scope.i = true;
+      $scope.$apply();
+      if ($scope.i){
+        $scope.i = false;
+        $scope.$apply();
+        window.location.reload(true);  
+      }*/    
+    }
+	
+})
